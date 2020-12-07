8637b960ade3eeb06fa68881e9c004f84d0ef080
Chai <chai@fy.com>
2020-12-07 10:08:00 +0800

    ordeing 更改banner

--- gpark/gordering/js/gordering.js
+++ gpark/gordering/js/gordering.js
@@ -2,14 +2,14 @@ require(['httpKit','echarts'], function (httpKit, echarts) {
     Vue.use(vant.Lazyload);
     new Vue({
         el: '#gordering',
-        template: `<div>
-                        <van-swipe :autoplay="3000">
+        template: `<div style="padding-bottom: 65px;">
+                        <van-swipe :autoplay="3000" style="margin:15px 15px 0;">
                           <van-swipe-item v-for="(image, index) in images" :key="index">
-                            <img v-lazy="image" style="width: 100%" />
+                            <van-image radius="15px" lazy-load :src="image" style="width: 100%" />
                           </van-swipe-item>
                         </van-swipe>
-                        <van-divider :style="{ borderColor: '#fff', padding: '0 10px',color:'#333' }"></van-divider>
-                        <van-grid :gutter="20" :column-num="2">
+                        <van-divider :style="{ borderColor: '#fff', padding: '0 5px',color:'#999',margin:'5px 0' }"></van-divider>
+                        <van-grid :gutter="15" :column-num="2">
                            <van-grid-item clickable  url="../gworkorder/gworkorder.html?type=3">
                             <van-icon name="todo-list-o" size="30" color="#d87a80"/>
                             <span style="color:#d87a80;font-size:14px">加班餐</span>
@@ -35,11 +35,9 @@ require(['httpKit','echarts'], function (httpKit, echarts) {
                             <van-icon name="more-o" size="30" color="#999"/>
                             <span style="color:#999;font-size:14px">更多</span>
                           </van-grid-item>
-                          <!--<van-grid-item clickable icon="vip-card-o" text="包厢/卡包" />
-                          <van-grid-item clickable icon="send-gift-o" text="商务套餐" />
-                          <van-grid-item clickable icon="todo-list-o" text="加班餐" />
-                          <van-grid-item clickable icon="more-o" text="更多" />-->
                         </van-grid>
+                        <van-divider :style="{ borderColor: '#fff', padding: '0 5px',color:'#999' }">系统测试中，未正式上线</van-divider>
+                        <!--<p style="color:#999;font-size:14px;text-align: center">系统测试中，未正式上线</p>-->
                         <div class="myorder">
                            <van-grid direction="horizontal" :column-num="1">
                               <van-grid-item url="../gmyordering/gmyordering.html" icon="orders-o" text="我的预定" />
@@ -50,7 +48,7 @@ require(['httpKit','echarts'], function (httpKit, echarts) {
             data() {
                 return {
                     images: [
-                        '../gordering/img/img1.png',
+                        '../gordering/img/pic-1.png',
                     ],
                 };
             },
