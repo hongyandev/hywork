c012e73501aff8e5f763c431c7b220ca32afc303
Chai <chai@fy.com>
2020-12-22 09:18:03 +0800

    新增今日菜单

--- gpark/gordering/js/gordering.js
+++ gpark/gordering/js/gordering.js
@@ -30,6 +30,11 @@ require(['httpKit','echarts'], function (httpKit, echarts) {
                             <van-icon name="balance-list-o" size="30" color="#d281ff"/>
                             <span style="color:#d281ff;font-size:14px">餐厅消费记录</span>
                           </van-grid-item>
+                         
+                          <van-grid-item clickable url="../gmenu/gmenu.html">
+                            <van-icon name="description"  size="30" color="#ff7fb1"/>
+                            <span style="color:#ff7fb1;font-size:14px">今日菜单</span>
+                          </van-grid-item>
                           <van-grid-item clickable>
                             <van-icon name="shop-o" size="30" color="#999"/>
                             <span style="color:#999;font-size:14px">小超市</span>
