a92eb558259918f4f187f422630165f134406495
Chai <chai@fy.com>
2021-01-20 11:27:23 +0800

    webjsbridge

--- gpark/source/js/webJsBridge.js
+++ gpark/source/js/webJsBridge.js
@@ -117,14 +117,14 @@ wbjsBridge.method.returnBack = function () {
  * @param funcCallback
  */
 wbjsBridge.method.getToken = function (funcCallback) {
-    funcCallback("eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI1MTI1IiwiZXhwIjoxNjExNjIyOTExLCJ1c2VySWQiOjUxMjUsImlhdCI6MTYxMTAxODExMSwiYWNjb3VudCI6IjAwMDAwMDAwLDExNzUzIiwidXNlcktleSI6Inh4eHgifQ._zd8rEhfcTXbd4OKOxnggrKygs0SaytPjP6DaAVaw-kNJI_3RKKr85S82KBwP5hsvk9yNf7mwEQUW6lXzQ8Qcg");
-   /* wbjsBridge.method.bridge.callHandler("getToken", {}, function (response) {
+    //funcCallback("eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI1MTI1IiwiZXhwIjoxNjExNjIyOTExLCJ1c2VySWQiOjUxMjUsImlhdCI6MTYxMTAxODExMSwiYWNjb3VudCI6IjAwMDAwMDAwLDExNzUzIiwidXNlcktleSI6Inh4eHgifQ._zd8rEhfcTXbd4OKOxnggrKygs0SaytPjP6DaAVaw-kNJI_3RKKr85S82KBwP5hsvk9yNf7mwEQUW6lXzQ8Qcg");
+    wbjsBridge.method.bridge.callHandler("getToken", {}, function (response) {
         console.info("getToken got response: ", response);
 
         if (typeof funcCallback == "function") {
             funcCallback(response);
         }
-    });*/
+    });
 }
 
 wbjsBridge.method.scanQRCode = function (funCallback) {
