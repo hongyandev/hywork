456be94b52c5bd2315603a2bb22b1f79a4a7e1fb
Chai <chai@fy.com>
2021-05-11 09:05:05 +0800

    gordering

--- gpark/gordering/js/gordering.js
+++ gpark/gordering/js/gordering.js
@@ -34,9 +34,9 @@ require(['httpKit','echarts'], function (httpKit, echarts) {
                             <van-icon name="description"  size="30" color="#ff7fb1"/>
                             <span style="color:#ff7fb1;font-size:14px">今日菜单</span>
                           </van-grid-item>
-                          <van-grid-item clickable url="../gcoffee/gcoffee.html">
-                            <van-icon class="iconfont icon-kafei"  size="30" color="#603811"/>
-                            <span style="color:#603811;font-size:14px">来杯咖啡</span>
+                          <van-grid-item v-show="showCoffee > 0 && showCoffee>5" clickable url="../gcoffee/gcoffee.html?showcart=2" >
+                            <van-icon class="iconfont icon-kafei"  size="30" color="#009e86"/>
+                            <span style="color:#009e86;font-size:14px">来杯咖啡</span>
                           </van-grid-item>
                           <van-grid-item clickable>
                             <van-icon name="shop-o" size="30" color="#999"/>
@@ -47,7 +47,7 @@ require(['httpKit','echarts'], function (httpKit, echarts) {
                             <span style="color:#999;font-size:14px">更多</span>
                           </van-grid-item>-->
                         </van-grid>
-                     
+                        <div class="cs" @click="gtap"></div>
                         <div class="myorder">
                            <van-grid direction="horizontal" :column-num="1">
                               <van-grid-item url="../gmyordering/gmyordering.html?type=jbc" icon="orders-o" text="我的预订" />
@@ -60,9 +60,13 @@ require(['httpKit','echarts'], function (httpKit, echarts) {
                     images: [
                         '../gordering/img/pic-1.png',
                     ],
+                    showCoffee: 0
                 };
             },
             methods: {
+                gtap(){
+                   this.showCoffee += 1;
+                },
                 grecord(){
                    // window.location.href = 'http://dev.sge.cn/rest/oapi/stxfjl?access_token='+document.cookie
 
